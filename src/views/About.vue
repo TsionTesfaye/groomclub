94eee0d5a45886495eb50afdc0aa94f9cf8a47c2
Tsion Tesfaye <tsitesfaye87466@gmail.com>
2024-02-20 14:41:37 +0300

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,163 @@
 <template>
-    <p> About </p>
-</template>
+    <div>
+        <p>We’re reimagining <span>grooming</span></p>
+
+        <p>Groom Club is the first mobile grooming service developed by veterinarians. We’re reimagining grooming to make it
+            easy for pet parents to give their best friend the long, healthy life they deserve.</p>
+
+        <button>Join our team</button>
+
+        <!-- image -->
+    </div>
+
+    <div>
+        <p>Join our team</p>
+    </div>
+
+    <p>Our guiding values</p>
+
+    <div>
+        <!-- image -->
+
+        <p>We get on the <span>floor</span></p>
+
+        <p>We believe that trust is earned, and we demonstrate this by getting on the floor. We establish a genuine
+            connection with our four-legged members and prioritize their needs.</p>
+
+    </div>
+
+    <div>
+        <p>Happy team, <span>healthy members</span></p>
+
+        <p>We champion the wellbeing and career growth of our team members. We build an environment founded on empathy, open
+            communication, and compassion.</p>
+
+        <!-- image -->
+
+    </div>
+
+    <div>
+        <!-- image -->
+
+        <p>Details <span>matter</span></p>
+
+        <p>Precision and attention to detail are our cornerstones. We establish clear expectations, follow through on
+            promises, and always act in the best interests of our members and their parents.</p>
+
+    </div>
+
+    <div>
+        <p>Own<span>it</span></p>
+
+        <p>Everything we do—from how we conduct ourselves with pet parents to the cleanliness of our facilities and
+            everything in between—is to provide the highest standard of care. When we encounter an unmet need, we step up
+            and own it.</p>
+
+        <!-- image -->
+
+    </div>
+
+    <div>
+        <!-- image -->
+
+        <p>Commited to our <span>community</span></p>
+
+        <p>We proudly support Nashville organizations that find loving homes for dogs, bring people together at community
+            events, and cultivate a workplace where veterinary professionals can enjoy fulfilling careers with a work-life
+            balance that respects their expertise and dedication</p>
+
+    </div>
+
+    <div>
+        <p>Groom Club</p>
+
+        <p>We’re committed to supporting organizations that find loving homes for our four-legged friends. Because we
+            believe that well-being is for all beings.</p>
+
+        <!-- image -->
+
+        <p>4 Good</p>
+
+    </div>
+
+    <div>
+        <!-- icon -->
+
+        <p>Comprehensive rewards</p>
+
+        <ul>
+            <li>Strong compensation</li>
+            <li>Comprehensive health benefits</li>
+            <li>Generous time off</li>
+            <li>Professional development opportunities</li>
+        </ul>
+
+    </div>
+
+    <div>
+        <!-- icon -->
+
+        <p>Work-life balance</p>
+
+        <ul>
+            <li>Consistent work schedule</li>
+            <li>28 paid days off per year</li>
+            <li>Team events</li>
+            <li>Empathetic culture</li>
+        </ul>
+
+    </div>
+
+    <div>
+        <!-- icon -->
+
+        <p>Career growth</p>
+
+        <ul>
+            <li>Veterinarian-developed training</li>
+            <li>Mentorship program</li>
+            <li>Specialized skills development</li>
+            <li>Advancement opportunities</li>
+        </ul>
+
+    </div>
+
+    <p>Happy team, <span>healthy members</span></p>
+
+    <p>Join our team and grow your career with our paid training and mentorship program, earn strong compensation that
+        respects your experience, and find work-life balance with 28 paid days off per year.</p>
+
+    <div>
+        <!-- small dog image -->
+
+        <p>"He loves hanging with his Groom Club friends and comes back smelling/looking better than ever."</p>
+
+        <p>Michael, Lemon's dad</p>
+    </div>
+
+    <div>
+        <p>Join our <span>team</span></p>
+
+        <p>If you share our belief that well-being is for all beings, join us in reimagining the grooming experience for
+            both our members and our team.</p>
+
+        <p>Don’t see a fit? Email us—</p>
+
+        <p>careers@groomclub.com</p>
+
+        <button>Dog Groomer (Total Care Partner)</button>
+        <button>Grooming Instructor</button>
+        <button>Member Experience Manager</button>
+
+    </div>
+
+
+    <div>
+        <p><span>Wellness-focused</span> grooming, delivered to your doorstep</p>
+
+    <!-- image -->
+
+    <button>Join the club</button>
+
+    <p>Call or text (615) 619-3647</p>
+</div></template>
