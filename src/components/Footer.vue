526cc178b8145f95fa2cbf9edb13c5ce8a1355c1
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-02-20 23:27:56 +0300

    add image in home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,25 @@
 <template>
     <!-- image -->
+    <div class="bg-shark text-white p-5">
 
-    <div>
-        <p>Sign up for our <span> newsletter </span></p>
-        <input placeholder="Enter your Email" type="email">
+        <img class="fill-sidecar mb-5" src="../assets/pictures/groomclub_gc.svg" />
 
-        <p>Find your way</p>
+        <p class="text-3xl my-5">Sign up for our <span class="font-bold"> newsletter </span></p>
+        
+        <input class="mb-8 p-5 rounded-xl bg-white placeholder:text-shark placeholder:font-semibold" placeholder="Enter your Email" type="email">
 
-        <ul>
+        <p class="text-3xl mb-5">Find your <span class="font-bold"> way </span></p>
+
+        <ul class="text-sidecar flex flex-col gap-5">
             <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+            <li>About</li>
+            <li>Privacy Policy</li>
+            <li>Terms of services</li>
         </ul>
         <!-- social icons -->
         <!-- image -->
+        <img class="w-full h-full" src="../assets/pictures/illofooter.png" alt=""> 
+
     </div>
 
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,14 +10,13 @@ import JoinButton from '../components/JoinButton.vue';
     
     <JoinButton />
     <!-- hero image -->
+    <img class="w-full h-full" src="../assets/pictures/groomclub-hero.png" />
     <p class="text-3xl text-center">The feeling is <span class="font-bold">mutual</span></p>
     <!-- slider -->
-    <!--  -->
-
     <div>
 
-        <div class="bg-white p-5 rounded-lg shadow-xl mx-5">
-            <!-- image -->
+        <div class="flex gap-2 items-center h-fit bg-white p-5 rounded-2xl shadow-xl mx-5">
+            <img class="w-auto h-14 rounded-full" src="../assets/pictures/Alyson-p-500.jpg" />
             <p class="text-shark font-bold">
                 "Groom Club is the best of ALL worlds if you're looking to get your dog the groom and love they deserve with the convenience of not having to leave your home."
                 Megan, Cinco's mom
@@ -55,6 +54,7 @@ import JoinButton from '../components/JoinButton.vue';
     </div>
 
         <!-- video -->
+        <img class="w-full h-full rounded-xl" src="../assets/pictures/dog_need_grooming.jpg" />
 
         <p class="text-shark text-3xl text-center p-6">Don't see your neighborhood? 
             <span class="font-bold"> Don't worry. </span>
@@ -69,20 +69,24 @@ import JoinButton from '../components/JoinButton.vue';
             <span class="text-sidecar font-bold text-xl">@groomclub4dogs</span>
         </Button>
 
-        <div class="bg-shark text-white_linen mx-8 my-7 rounded-3xl p-5 flex justify-center flex-col items-center">
+        <div class="bg-shark text-white_linen mx-8 my-7 rounded-3xl p-5 flex justify-center flex-col items-center text-center gap-5">
             <!-- image -->
+            <img class="w-full h-92 rounded-2xl" src="../assets/pictures/groomclub-15.webp" />
             <p class="text-sidecar text-5xl text-cente p-5"> Mobile grooming, <span class="font-bold block">totally reimagined </span></p>
             
-            <div class="text-center flex flex-col gap-4">
+            <div class="text-center flex flex-col gap-4 items-center">
                 <!-- icon  -->
+                <img class="w-24 h-auto" src="../assets/pictures/Frame 96.png" />
+
                 <h3 class="font-bold">Convenience for you </h3>
                 <p>
                     Give your dog a better life—without sacrificing your own. We make it easy with online scheduling, upfront pricing and convenient same-week appointments.
                 </p>
             </div>
             
-            <div class="text-center flex flex-col gap-4">
+            <div class="text-center flex flex-col gap-4 items-center">
                 <!-- icon  -->
+                <img class="w-24 h-auto" src="../assets/pictures/Frame 95.png" />
                 <h3>Wellness for your pup </h3>
                 <p>
                     We combine grooming, preventative care, and our nose-to-tail wellness check to keep your best friend in tip-top shape.                </p>
@@ -93,6 +97,15 @@ import JoinButton from '../components/JoinButton.vue';
         <h2 class="text-center text-3xl m-10">Well-being <span class="font-bold"> isn't </span> just for human beings.</h2>
 
         <!-- image x2 -->
+    <div class="relative m-5">
+
+        <img class="w-full h-auto" src="../assets/pictures/baily.svg" />
+            
+        <img class="w-full h-auto rounded-full" src="../assets/pictures/dog-text-p-800.png" />
+        
+    </div>
+
+
 
         <p class="text-2xl text-center m-10">Total <span class="font-bold"> Care </span> </p>
 
@@ -130,59 +143,72 @@ import JoinButton from '../components/JoinButton.vue';
             </h3>
 
             <p> Early detection is critical to managing diseases. Groom Club members see a veterinary professional up to 12 times more per year than non-members, increasing the chances of catching a health issue early. </p>
-        <div class="flex flex-wrap gap-8 relative m-8">    
-            
-            <div class="bg-white hover:bg-sidecar hover:border-2 hover:border-shark w-fit p-2 px-5 rounded-lg group">
-                <p class="p-3 rounded-md absolute hidden group-hover:block top-0  bg-shark text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est fugiat facilis exercitationem cupiditate? Quas omnis eos ipsam sequi deserunt. Vitae, laudantium. Ratione atque pariatur cumque asperiores voluptatem explicabo tenetur eum repellendus placeat, ipsum rerum vitae sapiente at quaerat eius obcaecati! Blanditiis assumenda dicta modi quibusdam explicabo? Dignissimos, nesciunt. Reiciendis, debitis.</p>
-                <div>Eye</div>
-            </div>
-            
-            <div >
-                <div>Eye</div>
-            </div>
-            
-            <div>
-                <div>Eye</div>
-            </div>  
 
-            <div>
-                <div>Eye</div>
-            </div> 
+            <div class="flex flex-wrap gap-8 relative m-8">    
+                
+                <div class="bg-white hover:bg-sidecar hover:border-2 hover:border-shark w-fit p-2 px-5 rounded-lg group">
+                    <p class="p-3 rounded-md absolute hidden group-hover:block top-0  bg-shark text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est fugiat facilis exercitationem cupiditate? Quas omnis eos ipsam sequi deserunt. Vitae, laudantium. Ratione atque pariatur cumque asperiores voluptatem explicabo tenetur eum repellendus placeat, ipsum rerum vitae sapiente at quaerat eius obcaecati! Blanditiis assumenda dicta modi quibusdam explicabo? Dignissimos, nesciunt. Reiciendis, debitis.</p>
+                    <div>Eye</div>
+                </div>
+                
+                <div >
+                    <div>Eye</div>
+                </div>
+                
+                <div>
+                    <div>Eye</div>
+                </div>  
 
-            <div>
-                <div>Eye</div>
-            </div> 
+                <div>
+                    <div>Eye</div>
+                </div> 
 
-            <div>
-                <div>Eye</div>
-            </div>  
+                <div>
+                    <div>Eye</div>
+                </div> 
 
-            <div>
-                <div>Eye</div>
-            </div>
+                <div>
+                    <div>Eye</div>
+                </div>  
 
-        </div>
-        <!-- image -->
+                <div>
+                    <div>Eye</div>
+                </div>
 
-            <div class="bg-shark p-5 text-xl text-center text-white rounded-lg">
                 <!-- image -->
-                <p>
-                    Stay informed with digital health reports and records
-                </p>
+                <div>
+                    <img class="w-full h-full" src="../assets/pictures/vet_illo.png" />
+                </div>
             </div>
 
-            <div>
-                <!-- image -->
-                <p>
-                    Don't worry about finding your vaccine info — we got you
-                </p>
-            </div>
+            <div class="flex flex-col gap-5">
 
-            <div>
-                <!-- image -->
-                <p>
-                    Rest easy knowing we're veterinary professionals
-                </p>
+                <div class="bg-shark p-5 text-2xl font-light text-center text-white rounded-lg flex flex-col gap-5 items-center">
+                    <!-- image -->
+                    <img class="w-24 h-auto" src="../assets/pictures/reportcard.png" />
+
+                    <p>
+                        Stay informed with digital health reports and records
+                    </p>
+                </div>
+
+                <div class="bg-shark p-5 text-2xl font-light text-center text-white rounded-lg flex flex-col gap-5 items-center">
+                    <!-- image -->
+                    <img class="w-24 h-auto" src="../assets/pictures/reportcard.png" />
+
+                    <p>
+                        Don't worry about finding your vaccine info — we got you
+                    </p>
+                </div>
+
+                <div class="bg-shark p-5 text-2xl font-light text-center text-white rounded-lg flex flex-col gap-5 items-center">
+                    <!-- image -->
+                    <img class="w-24 h-auto" src="../assets/pictures/reportcard.png" />
+
+                    <p>
+                        Rest easy knowing we're veterinary professionals
+                    </p>
+                </div>
             </div>
 
         </div>
@@ -190,18 +216,15 @@ import JoinButton from '../components/JoinButton.vue';
         <div class="bg-white shadow-xl rounded-3xl text-center p-6 m-6">
             <p class="text-3xl text-shark">We don't make you <span class="font-bold"> play fetch </span></p>
 
-            <div>
-                <!-- image -->
-               <p>
-                   150
-               </p>
-               <p>
-                premium mobile grooming without add-ons or upsells
-               </p> 
+            <div class=" border mx-10">
+                <img class="w-full h-auto" src="../assets/pictures/circle.svg" />
+
+                <p class="text-5xl font-bold text-shark">150</p>
+                <p class="text-sm"> premium mobile grooming without add-ons or upsells </p> 
             </div>
 
             <ul class="flex flex-col gap-4 text-start p-6">
-                <li> Same-week appointments </li>
+                <li> <img class="w-2 h-5 inline" src="../assets/pictures/right_mar.svg"> Same-week appointments </li>
                 <li> Stress-free grooming experience </li>
                 <li> Clean and safe environment </li>
                 <li> Premium grooming products </li>
@@ -222,17 +245,39 @@ import JoinButton from '../components/JoinButton.vue';
         <p class="font-2xl text-shark text-center m-5">We're reimagining grooming to make it easy for pet parents to give their best friend the long, healthy life they deserve.</p>
         
         
-        <div class="bg-white rounded-3xl shadow-xl flex flex-col justify-center items-center gap-5">
+        <div class="bg-white rounded-3xl shadow-xl flex flex-col justify-center items-center gap-5 m-5 p-5 overflow-hidden">
         <!-- image -->
+        <img class="w-[0.6] h-full" src="../assets/pictures/groomclub-compare.svg" />
+
         <p class="text-shark text-2xl text-center">Mobile grooming, <span class="font-bold"> totally reimagined </span></p>
-            <ul class="flex flex-col gap-5">
-                <li> Grooming focused on health </li>
-                <li> Delivered to your home </li>
-                <li> Same-week appointments </li>
-                <li> Grooming focused on health </li>
-                <li> Grooming focused on health </li>
-                <li> Grooming focused on health </li>
-                <li> Grooming focused on health </li>
+            <ul class="flex flex-col gap-5 font-semibold">
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Grooming focused on health 
+                </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Delivered to your home </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Same-week appointments 
+                </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Grooming focused on health 
+                </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Grooming focused on health 
+                </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Grooming focused on health 
+                </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-02.png" alt=""> 
+                    Grooming focused on health 
+                </li>
             </ul>
             <JoinButton />
         </div>
@@ -243,72 +288,61 @@ import JoinButton from '../components/JoinButton.vue';
             </p>
 
             <ul class="font-bold flex flex-col gap-5 text-base">
-                <li> Grooming focused on health </li>
-                <li> Delivered to your home </li>
-                <li> Same-week appointments </li>
-                <li> Grooming focused on health </li>
-                <li> Grooming focused on health </li>
-                <li> Grooming focused on health </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Grooming focused on health </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Delivered to your home </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Same-week appointments </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Grooming focused on health </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Grooming focused on health </li>
+                <li class="flex gap-2 items-center "> 
+                    <img class="w-12 h-auto inline" src="../assets/pictures/check-no.png" alt=""> 
+                    Grooming focused on health </li>
             </ul>
         </div>
 
-        <h2 class="text-center p-5 text-2xl">
-            Have a question? <span class="font-bold">We got you</span>
+        <h2 class="text-center p-5 m-5 text-3xl">
+            Have a question? <span class="font-bold block">We got you</span>
         </h2>
 
-        <div class="">
-        <!-- image -->
-        <p class="bg-beryl_green w-fit p-5 rounded-full rounded-tr-none">
+        <div class="flex gap-2 bg-beryl_green m-auto rounded-full rounded-tr-none w-fit p-5 ">
+
+        <img class="w-12 h-auto" src="../assets/pictures/callus-groom.png" alt=""> 
+        
+        <p class=" ">
             Call or text (615) 619-DOGS
-            <span class="block  text-base font-bold">Available now!</span>
+            <span class="block  text-base font-bold m-5">Available now!</span>
         </p>
         </div>
 
-                <!-- image -->
+        <img class="w-2/3 h-full m-auto" src="../assets/pictures/manicure.png" alt=""> 
 
         <h2 class="font-bold text-3xl text-center m-5 text-shark">FAQ</h2>
         
-        <div class="group bg-shark text-white text-2xl font-bold text-start p-6 m-5 rounded-3xl">
-            <h3>What is Groom Club</h3>
+        <button class="group bg-shark text-white text-2xl font-bold text-start p-6 m-5 rounded-3xl flex items-center justify-center">
+            
+            <h3 class="block">What is Groom Club</h3>
             <p class="hidden group-focus:block">
                 Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
             </p>
-        </div>
-        <div>
-            <h3>What is groom club</h3>
-            <p>
-                Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
-            </p>
-        </div>
-        <div>
-            <h3>What is groom club</h3>
-            <p>
-                Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
-            </p>
-        </div>
-        <div>
-            <h3>What is groom club</h3>
-            <p>
-                Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
-            </p>
-        </div>
-        <div>
-            <h3>What is groom club</h3>
-            <p>
-                Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
-            </p>
-        </div>
-        <div>
-            <h3>What is groom club</h3>
-            <p>
-                Groom Club provides wellness-focused mobile grooming services, developed by vets, delivered to your doorstep.
-            </p>
-        </div>
+            <img class="w-5 h-auto m-auto fill-white" src="../assets/pictures/downward.svg" alt=""> 
 
-        <div class="bg-sidecar m-5 p-5 rounded-lg flex flex-col items-center gap-5">
+        </button>
+
+        <div class="bg-sidecar mx-5 p-5 rounded-lg rounded-b-none flex flex-col items-center gap-5">
             <h2 class="text-3xl text-center"> <span class="font-bold"> Wellness-focused </span> grooming, delivered to your doorstep</h2>
 
             <!-- image -->
+            <img class="w-full h-full m-auto" src="../assets/pictures/Dog_Footer.png" /> 
+
 
             <JoinButton />
 
