c864cc271982b46103e4a36f50c6be818b7dcaba
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-02-20 13:45:05 +0300

    skeleton home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js,vue,ts}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        white:"#ffffff",
+        spring_wood:"#f7f4f0",
+        white_linen:"#fcf9f5",
+        shark:"#2e2f33",
+        sidecar:"#f3e6bc",
+        beryl_green:"#dce5b8",
+        zinnwaldite:"#eacbac",
+      }
+    },
   },
   plugins: [],
 }
